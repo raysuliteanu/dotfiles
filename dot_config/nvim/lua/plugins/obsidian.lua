7f8d571af36da660a91f99e381b286c3775df2a8
Ray Suliteanu <raysuliteanu@gmail.com>
2024-10-07 14:08:35 -0700

    Update .config/nvim/lua/plugins/obsidian.lua

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -37,6 +37,22 @@ return {
 						name = "Ray",
 						path = "/home/ray/Documents/Obsidian/Ray",
 					},
+					{
+						name = "no-vault",
+						path = function()
+							-- alternatively use the CWD:
+							-- return assert(vim.fn.getcwd())
+							return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+						end,
+						overrides = {
+							notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of 'nil'
+							new_notes_location = "current_dir",
+							templates = {
+								folder = vim.NIL,
+							},
+							disable_frontmatter = true,
+						},
+					},
 				},
 				notes_subdir = "Resources/Notes",
 				new_notes_location = "notes_subdir",
