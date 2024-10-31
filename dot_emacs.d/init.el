eaba25323d7b870af41208172dfb3d1993f5300f
Ray Suliteanu <raysuliteanu@gmail.com>
2024-10-31 10:36:11 -0700

    Update .emacs.d/init.el

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -1,10 +1,13 @@
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (package-refresh-contents)
 
-(setq catppuccin-flavor 'latte) ; or 'latte, 'macchiato, or 'mocha
+(setq catppuccin-flavor 'mocha) ; or 'latte, 'macchiato, or 'mocha
 (load-theme 'catppuccin t)
 
 (custom-set-variables
@@ -12,7 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(slime rust-mode)))
+ '(package-selected-packages '(which-key slime rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -20,3 +23,5 @@
  ;; If there is more than one, they won't work right.
  )
 (setq inferior-lisp-program "clisp")
+
+(which-key-mode 1)
