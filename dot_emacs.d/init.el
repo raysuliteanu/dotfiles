85b1ca147df1a65900543302ce967b7cd0787853
Ray Suliteanu <raysuliteanu@gmail.com>
2024-10-31 11:41:41 -0700

    Update .emacs.d/init.el

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -15,7 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(which-key slime rust-mode)))
+ '(package-selected-packages '(flycheck lsp-mode which-key slime rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,3 +25,6 @@
 (setq inferior-lisp-program "clisp")
 
 (which-key-mode 1)
+
+(global-flycheck-mode +1)
+
