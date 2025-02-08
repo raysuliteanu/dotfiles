2387c97d023584bae1012ad5e3b992c8d857db51
Ray Suliteanu <raysuliteanu@gmail.com>
2025-02-08 10:40:56 -0800

    Update .config/nvim/lua/plugins/cloak.lua

--- dot_config/nvim/lua/plugins/cloak.lua
+++ dot_config/nvim/lua/plugins/cloak.lua
@@ -20,7 +20,7 @@ return {
 					{
 						-- Match any file starting with '.env'.
 						-- This can be a table to match multiple file patterns.
-						file_pattern = ".env*",
+						file_pattern = { ".env*", ".properties" },
 						-- Match an equals sign and any character after it.
 						-- This can also be a table of patterns to cloak,
 						cloak_pattern = { "=.+", ":.+" },
