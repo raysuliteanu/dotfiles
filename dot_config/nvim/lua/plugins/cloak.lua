2704e4b2926d1919614b3d1aa6bb208afc0b20fb
Ray Suliteanu <raysuliteanu@gmail.com>
2025-02-08 10:37:47 -0800

    Update .config/nvim/lua/plugins/cloak.lua

--- dot_config/nvim/lua/plugins/cloak.lua
+++ dot_config/nvim/lua/plugins/cloak.lua
@@ -23,7 +23,7 @@ return {
 						file_pattern = ".env*",
 						-- Match an equals sign and any character after it.
 						-- This can also be a table of patterns to cloak,
-						cloak_pattern = { "=.+", ":.+", "-.+" },
+						cloak_pattern = { "=.+", ":.+" },
 						-- A function, table or string to generate the replacement.
 						-- The actual replacement will contain the 'cloak_character'
 						-- where it doesn't cover the original text.
