aba5f39074107d610c9e7d15f501fea8ddda52b7
Ray Suliteanu <raysuliteanu@gmail.com>
2025-02-15 11:48:08 -0800

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -41,3 +41,10 @@ vim.keymap.set("n", "gf", function()
 		return "gf"
 	end
 end, { noremap = false, expr = true })
+
+vim.keymap.set(
+	"v",
+	"<C-t>",
+	":Silicon<CR>",
+	{ noremap = false, silent = false, desc = "[t]ake screenshot from selection" }
+)
