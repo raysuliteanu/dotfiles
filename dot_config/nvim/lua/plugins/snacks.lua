9914c07ba83c5cf531518ba5a541979fcf2adbe4
Ray Suliteanu <raysuliteanu@gmail.com>
2025-02-17 15:45:00 -0800

    Add .config/nvim/lua/plugins/snacks.lua

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"folke/snacks.nvim",
+		opts = {
+			image = {
+				-- your image configuration comes here
+				-- or leave it empty to use the default settings
+				doc = {
+					inline = false,
+				},
+			},
+		},
+	},
+}
