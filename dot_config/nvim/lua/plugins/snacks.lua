91830297f0fa47eb412e0fde7ecb5062829ef8ab
Ray Suliteanu <raysuliteanu@gmail.com>
2025-02-23 13:27:04 -0800

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -26,18 +26,58 @@ return {
 					{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 					{
 						pane = 2,
-						icon = " ",
-						title = "Git Status",
-						section = "terminal",
 						enabled = function()
 							return Snacks.git.get_root() ~= nil
 						end,
-						cmd = "git status --short --branch --renames",
-						height = 5,
+						icon = " ",
+						desc = "Browse Repo",
 						padding = 1,
-						ttl = 5 * 60,
-						indent = 3,
+						key = "b",
+						action = function()
+							Snacks.gitbrowse()
+						end,
 					},
+					function()
+						local in_git = Snacks.git.get_root() ~= nil
+						local cmds = {
+							{
+								title = "Open Issues",
+								cmd = "gh issue list -L 3",
+								key = "i",
+								action = function()
+									vim.fn.jobstart("gh issue list --web", { detach = true })
+								end,
+								icon = " ",
+								height = 7,
+							},
+							{
+								icon = " ",
+								title = "Open PRs",
+								cmd = "gh pr list -L 3",
+								key = "P",
+								action = function()
+									vim.fn.jobstart("gh pr list --web", { detach = true })
+								end,
+								height = 7,
+							},
+							{
+								icon = " ",
+								title = "Git Status",
+								cmd = "git --no-pager diff --stat -B -M -C",
+								height = 10,
+							},
+						}
+						return vim.tbl_map(function(cmd)
+							return vim.tbl_extend("force", {
+								pane = 2,
+								section = "terminal",
+								enabled = in_git,
+								padding = 1,
+								ttl = 5 * 60,
+								indent = 3,
+							}, cmd)
+						end, cmds)
+					end,
 					{ section = "startup" },
 				},
 			},
