1e6ca75189a7e342331b6fe80b81cab4ae611356
Ray Suliteanu <raysuliteanu@gmail.com>
2025-04-01 12:19:49 -0700

    Add bin/new-blog-post.sh

---
+++ bin/executable_new-blog-post.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# a simple script to create a new markdown file with the
+# frontmatter from the template file I created.
+
+if [ $# -eq 0 ]; then
+	echo "blog file name must be provided"
+	exit 1
+fi
+
+# allow spaces in the script argument, but translate spaces to dashes
+FILE_NAME=$(echo "$*" | tr ' ' '-').md
+
+OBS_VAULT=$HOME/Documents/kidoni.dev
+TEMPLATE=$OBS_VAULT/blog-template.md
+BLOG_FILE=$OBS_VAULT/content/$FILE_NAME
+
+vi +":r $TEMPLATE" "$BLOG_FILE"
