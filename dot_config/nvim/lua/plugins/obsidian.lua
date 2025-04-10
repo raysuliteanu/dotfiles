952e8c8e3070e71bc4c64d69d18e8a6127d441b4
Ray Suliteanu <raysuliteanu@gmail.com>
2025-04-10 15:29:01 -0700

    Update .config/nvim/lua/plugins/obsidian.lua

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,103 +1,4 @@
--- event = {
--- 	-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
--- 	-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
--- 	-- refer to `:h file-pattern` for more examples
--- 	"BufReadPre "
--- 		.. vpath
--- 		.. "/**/*.md",
--- 	"BufNewFile " .. vpath .. "/**/*.md",
--- },
 local vpath = vim.fn.expand("~") .. "/Documents/Obsidian/Ray"
--- return {
--- 	{
--- 		"epwalsh/obsidian.nvim",
--- 		version = "*",
--- 		lazy = true,
--- 		-- ft = "markdown",
--- 		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
--- 		event = {
--- 			-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
--- 			-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
--- 			-- refer to `:h file-pattern` for more examples
--- 			"BufReadPre "
--- 				.. vpath
--- 				.. "/**/*.md",
--- 			"BufNewFile " .. vpath .. "/**/*.md",
--- 		},
--- 		dependencies = {
--- 			"echasnovski/mini.pick",
--- 		},
--- 		config = function()
--- 			require("obsidian").setup({
--- 				workspaces = {
--- 					{
--- 						name = "Ray",
--- 						path = vpath,
--- 					},
--- 				},
--- 				notes_subdir = "Resources/Notes",
--- 				new_notes_location = "notes_subdir",
--- 				disable_frontmatter = false,
--- 				templates = {
--- 					folder = "Templates",
--- 					date_format = "%Y-%m-%d",
--- 					time_format = "%H:%M",
--- 					substitutions = {},
--- 				},
--- 				completion = {
--- 					nvim_cmp = false,
--- 					min_chars = 2,
--- 				},
--- 				ui = {
--- 					-- using MeanderingProgrammer/render-markdown.nvim
--- 					-- see plugins/markdown.lua
--- 					enable = false,
--- 				},
--- 				picker = {
--- 					-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
--- 					name = "mini.pick",
--- 				},
--- 			})
--- 		end,
--- 		-- defaults to [n]ormal mode if not specified
--- 		keys = {
--- 			{
--- 				"<leader>Ov",
--- 				":cd " .. vpath .. "<CR>:ObsidianQuickSwitch<CR>",
--- 				desc = "Open Obsidian vault",
--- 			},
--- 			{
--- 				"<leader>On",
--- 				":ObsidianNewFromTemplate note<CR>",
--- 				desc = "New Obsidian note",
--- 			},
--- 			{
--- 				"<leader>Of",
--- 				":ObsidianQuickSwitch<CR>",
--- 				desc = "Find in Obsidian vault",
--- 			},
--- 			{
--- 				"<leader>Os",
--- 				":ObsidianSearch<CR>",
--- 				desc = "Search in Obsidian vault",
--- 			},
--- 			{
--- 				"<leader>Oe",
--- 				":ObsidianExtractNote<CR>",
--- 				desc = "Extract selected text to file",
--- 			},
--- 			{
--- 				"<leader>Od",
--- 				":ObsidianToday<CR>",
--- 				desc = "New/Open daily note",
--- 			},
--- 			{
--- 				"<leader>Ot",
--- 				":ObsidianTOC<CR>",
--- 				desc = "Open TOC for current file",
--- 			},
--- 		},
--- 	},
 return {
 	"obsidian-nvim/obsidian.nvim",
 	version = "*", -- recommended, use latest release instead of latest commit
@@ -130,6 +31,14 @@ return {
 			time_format = "%H:%M",
 			substitutions = {},
 		},
+		---@param url string
+		follow_url_func = function(url)
+			-- Open the URL in the default web browser.
+			-- vim.fn.jobstart({ "open", url }) -- Mac OS
+			-- vim.fn.jobstart({"xdg-open", url})  -- linux
+			-- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+			vim.ui.open(url) -- need Neovim 0.10.0+
+		end,
 		-- ui = {
 		-- 	-- using MeanderingProgrammer/render-markdown.nvim
 		-- 	-- see plugins/markdown.lua
