5bf537c8d97a32c9a02bb0c3f0aa3fa903a7bf06
Ray Suliteanu <raysuliteanu@gmail.com>
2025-04-10 15:20:10 -0700

    Update .config/nvim/lua/plugins/obsidian.lua

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -121,20 +121,38 @@ return {
 		},
 		notes_subdir = "Resources/Notes",
 		new_notes_location = "notes_subdir",
+		open_notes_in = "hsplit",
 		disable_frontmatter = false,
-		preferred_link_style = "wiki",
+		preferred_link_style = "markdown",
 		templates = {
 			folder = "Templates",
 			date_format = "%Y-%m-%d",
 			time_format = "%H:%M",
 			substitutions = {},
 		},
-
+		-- ui = {
+		-- 	-- using MeanderingProgrammer/render-markdown.nvim
+		-- 	-- see plugins/markdown.lua
+		-- 	enable = false,
+		-- },
+		picker = {
+			-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+			name = "snacks.pick",
+		},
 		completion = {
 			nvim_cmp = false,
 			blink = true,
 			-- Trigger completion at n chars.
 			min_chars = 3,
+		},
+	},
+	mappings = {
+		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+		["gf"] = {
+			action = function()
+				return require("obsidian").util.gf_passthrough()
+			end,
+			opts = { noremap = false, expr = true, buffer = true },
 		},
 	},
 	keys = {
