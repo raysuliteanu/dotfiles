c99c0341f8251c7aa9ff9e48cbc4b6fbc2667ece
Ray Suliteanu <raysuliteanu@gmail.com>
2025-04-12 14:12:24 -0700

    Update .config/nvim/lua/config/lazy.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -33,13 +33,13 @@ require("lazy").setup({
 		lazy = false,
 		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
 		-- have outdated releases, which may break your Neovim install.
-		version = false, -- always use the latest git commit
-		-- version = "*", -- try installing the latest stable version for plugins that support semver
+		-- version = false, -- always use the latest git commit
+		version = "*", -- try installing the latest stable version for plugins that support semver
 	},
 	checker = {
 		enabled = true, -- check for plugin updates periodically
 		notify = true, -- notify on update
-	}, -- automatically check for plugin updates
+	},
 	performance = {
 		rtp = {
 			-- disable some rtp plugins
