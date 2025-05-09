d191fe041e257707949aa2f63616f281dd96a37b
Ray Suliteanu <raysuliteanu@gmail.com>
2025-05-09 09:23:07 -0700

    Update .config/nvim/lua/config/lazy.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "catppuccin-latte",
+				colorscheme = "catppuccin-mocha",
 			},
 		},
 
