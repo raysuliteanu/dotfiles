170f2aaf343699eb0d6ce58a4081aba5550a0341
Ray Suliteanu <raysuliteanu@gmail.com>
2025-05-10 12:05:58 -0700

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -4,13 +4,13 @@
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-	pattern = "*",
-})
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+-- 	callback = function()
+-- 		vim.highlight.on_yank()
+-- 	end,
+-- 	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+-- 	pattern = "*",
+-- })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
