c06efb3823cab8509d270a28d74699b9a04eb388
Ray Suliteanu <raysuliteanu@gmail.com>
2025-05-15 11:01:14 -0700

    Update .config/nvim/lua/plugins/toggleterm.lua

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -1,6 +1,13 @@
 return {
 	-- amongst your other plugins
-	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = true,
+		opts = {
+			open_mapping = [[c-t]],
+		},
+	},
 	-- or
 	-- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
 }
