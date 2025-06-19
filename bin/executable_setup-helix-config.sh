8bc0f1b4cbaaa033ab638f72fa6d50af29bb3b15
Ray Suliteanu <raysuliteanu@gmail.com>
2025-06-19 16:25:02 -0700

    Add bin/setup-helix-config.sh

---
+++ bin/executable_setup-helix-config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <rust|mvn|gradle>"
+    exit 1
+fi
+
+case "$1" in
+    rust|mvn|gradle)
+        config_type="$1"
+        ;;
+    *)
+        echo "Error: Invalid argument. Must be one of: rust, mvn, gradle"
+        exit 1
+        ;;
+esac
+
+mkdir -p .helix
+
+ln -sf ~/src/.helix/config-${config_type}.toml .helix/config.toml
+
+echo "Created symlink .helix/config.toml -> ~/src/.helix/config-${config_type}.toml"
