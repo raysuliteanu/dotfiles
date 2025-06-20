ecd4b40fb35296a9f5b66d60132817b297cce2fe
Ray Suliteanu <raysuliteanu@gmail.com>
2025-06-20 14:29:17 -0700

    Add .config/nvim/lua/plugins/mason.lua

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "mason-org/mason-lspconfig.nvim",
+        version = "1.32.0",
+        opts = {
+            ensure_installed = { "lua_ls", "rust_analyzer", "jdtls" },
+        },
+        dependencies = {
+            { "mason-org/mason.nvim", version = "1.11.0" },
+            "neovim/nvim-lspconfig",
+        },
+    },
+}
