4c49cad798e43d4ef89c1290d2865a94e1afb991
Ray Suliteanu <raysuliteanu@gmail.com>
2025-07-05 10:47:37 -0700

    Update .config/nvim/lua/plugins/mason.lua Add .gemini/settings.json

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -3,7 +3,7 @@ return {
         "mason-org/mason-lspconfig.nvim",
         version = "1.32.0",
         opts = {
-            ensure_installed = { "lua_ls", "rust_analyzer", "jdtls" },
+            ensure_installed = { "lua_ls", "rust_analyzer" },
         },
         dependencies = {
             { "mason-org/mason.nvim", version = "1.11.0" },
