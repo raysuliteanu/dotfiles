34c6f8a492a31a12ecd7090a3ca2e1d3d35d2f15
Ray Suliteanu <raysuliteanu@gmail.com>
2025-08-04 10:11:56 -0700

    Update .config/nvim/lua/plugins/blink-cmp.lua

--- dot_config/nvim/lua/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/plugins/blink-cmp.lua
@@ -1,22 +1,13 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		"Kaiser-Yang/blink-cmp-avante",
-	},
-	version = "1.*",
-	opts = {
-		signature = { enabled = true },
-		sources = {
-			-- Add 'avante' to the list
-			default = { "avante", "lsp", "path", "snippets", "buffer" },
-			providers = {
-				avante = {
-					module = "blink-cmp-avante",
-					name = "Avante",
-					opts = {},
-				},
-			},
-		},
-	},
+    "saghen/blink.cmp",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+    },
+    version = "1.*",
+    opts = {
+        signature = { enabled = true },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+        },
+    },
 }
