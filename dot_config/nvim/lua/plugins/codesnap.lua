4dc3173c660b51e7b6efff74b5fb7ae896236c8e
Ray Suliteanu <raysuliteanu@gmail.com>
2025-11-19 15:07:15 -0800

    Add .config/nvim/lua/plugins/codesnap.lua

---
+++ dot_config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "mistricky/codesnap.nvim",
+        build = "make",
+        opts = {
+            bg_padding = 0,
+            save_path = "~/Pictures/Screenshots/",
+            has_line_number = true,
+            mac_window_bar = false,
+        },
+    },
+}
