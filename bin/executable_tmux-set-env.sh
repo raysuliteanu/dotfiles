b4aabd30ec1908bd86486825f32fbd5b67ee3a84
Ray Suliteanu <raysuliteanu@gmail.com>
2026-03-30 10:35:35 -0700

    Add bin/tmux-set-env.sh

---
+++ bin/executable_tmux-set-env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: tmux-set-env.sh VAR VALUE" >&2
+    exit 1
+fi
+
+tmux set-environment "$1" "$2"
+echo "Set $1 in tmux environment. Run 'export $1=$2' in existing panes."
