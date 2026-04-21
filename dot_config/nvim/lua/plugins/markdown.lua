7c4a0f7bd4e5faf788bcb95ab71c648d457821d4
Ray Suliteanu <raysuliteanu@gmail.com>
2026-04-21 07:37:04 -0700

    Update .config/nvim/lua/plugins/markdown.lua

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,21 +1,26 @@
 return {
+    -- render-markdown.nvim is managed by the LazyVim markdown extra.
+    -- This override just adds blink.cmp completions support.
     {
         "MeanderingProgrammer/render-markdown.nvim",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons",
-        },
-        ft = { "markdown" },
         opts = {
             completions = { blink = { enabled = true } },
         },
     },
+
+    -- Drop prettier from the markdown formatter chain:
+    --   - prettier is not installed
+    --   - line-wrapping is handled by the BufWritePre gw autocmd (autocmds.lua)
+    --   - markdownlint-cli2 auto-fixes lint issues on save
+    --   - markdown-toc updates TOC markers when present
+    -- markdown-preview.nvim is fully managed by the LazyVim markdown extra.
     {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                markdown = { "markdownlint-cli2", "markdown-toc" },
+                ["markdown.mdx"] = { "markdownlint-cli2", "markdown-toc" },
+            },
+        },
     },
 }
